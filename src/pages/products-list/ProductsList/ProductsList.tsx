7ebdc58c70da5272afc8f4ca3ba7849da7fb9d70
MihaiDragutescu <mihaidragutescu36@gmail.com>
2023-01-19 18:12:19 +0200

    Added price range lider component

--- src/pages/products-list/ProductsList/ProductsList.tsx
+++ src/pages/products-list/ProductsList/ProductsList.tsx
@@ -1,24 +1,12 @@
 import './ProductsList.scss';
-import ColorPill from '@Components/ui/ColorPill';
+import PriceSlider from '../PriceRangeSlider/PriceRangeSlider';
 
 const ProductsList: React.FC = () => {
   return (
     <div className='products-list'>
       <div className='products-list__container ssh-container'>
         <div className='products-list__row ssh-row'>
-          <ColorPill
-            handleClick={() => {
-              console.log('ghj');
-            }}
-            color='#2127AF'
-          />
-          <ColorPill
-            handleClick={() => {
-              console.log('ghj');
-            }}
-            color='#2127AF'
-            active
-          />
+          <PriceSlider />
         </div>
       </div>
     </div>
