e90275f9bc37ac0ede4489256bcc5912149eecc2
MihaiDragutescu <mihaidragutescu36@gmail.com>
2023-01-24 13:35:24 +0200

    Added accordion component

--- src/components/ui/Accordion.tsx
+++ src/components/ui/Accordion.tsx
@@ -1,7 +1,46 @@
+import { ReactComponent as ArrowDown } from '@Assets/images/accordion-arrow-down.svg';
+import { ReactComponent as ArrowUp } from '@Assets/images/accordion-arrow-up.svg';
+import { useState } from 'react';
 import './Accordion.scss';
 
-const Accordion: React.FC = () => {
-  return <div className='ssh-accordion'>123</div>;
+interface AccordionProps {
+  accordionList: { label: string; content: string }[];
+}
+
+const Accordion: React.FC<AccordionProps> = (props: AccordionProps) => {
+  const [expandedIndex, setExpandedIndex] = useState(-1);
+  const handleClick = (nextIndex: number) => {
+    setExpandedIndex((currentIndex) => {
+      if (currentIndex === nextIndex) {
+        return -1;
+      } else {
+        return nextIndex;
+      }
+    });
+  };
+
+  const list = props.accordionList.map((item, index) => {
+    const isExpanded = expandedIndex === index;
+
+    return (
+      <div
+        key={index}
+        className='ssh-accordion__item'
+        onClick={() => {
+          handleClick(index);
+        }}
+      >
+        <div className='ssh-accordion__label'>
+          {item.label} {isExpanded ? <ArrowUp /> : <ArrowDown />}
+        </div>
+        <div className={`ssh-accordion__content ${isExpanded ? '' : 'hidden'}`}>
+          {item.content}
+        </div>
+      </div>
+    );
+  });
+
+  return <div className='ssh-accordion'>{list}</div>;
 };
 
 export default Accordion;
--- src/pages/product/ProductPage.tsx
+++ src/pages/product/ProductPage.tsx
@@ -1,7 +1,29 @@
 import Accordion from '@Components/ui/Accordion';
 
 const ProductsPage = () => {
-  return <Accordion />;
+  const accordionList = [
+    {
+      label: 'Size & Fit',
+      content:
+        "Luxury meets utility in the clean-lined Alby Jacket. It's supremely soft and light in Italian cotton garment washed to achieve a smooth, sueded finish while the relaxed fit and boxy shape infuse the look with a modern sensibility.",
+    },
+    {
+      label: 'Shipping Instructions',
+      content:
+        "Luxury meets utility in the clean-lined Alby Jacket. It's supremely soft and light in Italian cotton garment washed to achieve a smooth, sueded finish while the relaxed fit and boxy shape infuse the look with a modern sensibility.",
+    },
+    {
+      label: 'Fit & Size',
+      content:
+        "Luxury meets utility in the clean-lined Alby Jacket. It's supremely soft and light in Italian cotton garment washed to achieve a smooth, sueded finish while the relaxed fit and boxy shape infuse the look with a modern sensibility.",
+    },
+  ];
+
+  return (
+    <div style={{ maxWidth: '710px' }}>
+      <Accordion accordionList={accordionList} />
+    </div>
+  );
 };
 
 export default ProductsPage;
