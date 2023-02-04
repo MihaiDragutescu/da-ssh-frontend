1d252ded0ded84ae5f206647cb0b9b015341486c
MihaiDragutescu <mihaidragutescu36@gmail.com>
2023-02-04 17:06:46 +0200

    Update button component and improve html semantics

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -3,13 +3,15 @@ import './Button.scss';
 interface ButtonProps {
   className?: string;
   children?: React.ReactNode;
-  onClick: () => void;
+  onClick?: () => void;
+  type?: 'submit' | 'button';
 }
 
 const Button: React.FC<ButtonProps> = (props: ButtonProps) => {
   return (
     <button
       className={`ssh-button ${props.className ?? ''}`}
+      type={props.type ?? 'button'}
       onClick={props.onClick}
     >
       {props.children}
