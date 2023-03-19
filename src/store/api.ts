6e9b37e616be5038bbaba15b150773038d033fa1
Mihai Dragutescu <mihai@codesilk.com>
2023-03-19 02:48:34 +0200

    Remove Procfile and server.js

--- src/store/api.ts
+++ src/store/api.ts
@@ -8,7 +8,7 @@ import { isStringArray } from '@Utils/isTypeGuards';
 
 export const sshApi = createApi({
   reducerPath: 'sshApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://da-ssh-app.herokuapp.com' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://da-ssh-server.herokuapp.com' }),
   endpoints: (builder) => {
     return {
       getAllProducts: builder.query<ProductType[], void>({
